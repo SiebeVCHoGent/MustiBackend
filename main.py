4c8a5e1ad2f76355fc6e72f262fc037874625cda
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-04-28 14:02:19 +0200

    CORS added

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+import random
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/musti")
 def read_root():
-    return {"Hello": "World"}
+    statusses = ['Aanwezig', "Niet aanwezig", "Op weg naar buiten"]
+
+    return {"status": random.choice(statusses)}
